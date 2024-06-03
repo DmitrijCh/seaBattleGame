7974fbdf1291ef580bce3e1b4726b8e120998258
dmitrij <dmitrij.chvanov@gmail.com>
2024-06-03 19:21:22 +0400

    Fixed time zone

--- src/main/java/org/dmitrijch/entity/Game.java
+++ src/main/java/org/dmitrijch/entity/Game.java
@@ -1,5 +1,6 @@
 package org.dmitrijch.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,10 +13,12 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
     private LocalDateTime startTime;
 
     public Game() {
-        this.startTime = LocalDateTime.now();
+        this.startTime = LocalDateTime.now().withNano(0);
     }
 
     public Long getId() {
--- src/main/java/org/dmitrijch/service/GameService.java
+++ src/main/java/org/dmitrijch/service/GameService.java
@@ -2,19 +2,16 @@ package org.dmitrijch.service;
 
 import org.dmitrijch.entity.Game;
 import org.dmitrijch.repository.GameRepository;
-import org.dmitrijch.repository.ShipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
     private final GameRepository gameRepository;
-    private final ShipRepository shipRepository;
 
     @Autowired
-    public GameService(GameRepository gameRepository, ShipRepository shipRepository) {
+    public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
-        this.shipRepository = shipRepository;
     }
 
     public Game startNewGame() {
