c6f8324d4343b9b11d84b2fc904d07a8d86f5855
dmitrij <dmitrij.chvanov@gmail.com>
2024-06-03 17:24:34 +0400

    Added exception

---
+++ src/main/java/org/dmitrijch/exeption/ShipPlacementException.java
@@ -0,0 +1,7 @@
+package org.dmitrijch.exeption;
+
+public class ShipPlacementException extends RuntimeException {
+    public ShipPlacementException(String message) {
+        super(message);
+    }
+}
