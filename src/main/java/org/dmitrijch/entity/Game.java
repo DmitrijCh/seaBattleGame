8e3aa3e99758c6fe4c6c21fe1628fbb406de05c3
dmitrij <dmitrij.chvanov@gmail.com>
2024-06-03 17:14:37 +0400

    Added entity

---
+++ src/main/java/org/dmitrijch/entity/Game.java
@@ -0,0 +1,36 @@
+package org.dmitrijch.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime startTime;
+
+    public Game() {
+        this.startTime = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+}
---
+++ src/main/java/org/dmitrijch/entity/Ship.java
@@ -0,0 +1,69 @@
+package org.dmitrijch.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Ship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id")
+    private Player player;
+
+    private int shipType;
+    @Column(name = "position_x")
+    private String positionX;
+    @Column(name = "position_y")
+    private int positionY;
+    private String orientation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(int shipType) {
+        this.shipType = shipType;
+    }
+
+    public String getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(String positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
